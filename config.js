1f5986f91b660ee12539badee5f176f814497cdb
sofina-dev <81674971+sofina-dev@users.noreply.github.com>
2026-04-12 08:55:35 +0700

    Modify GAS_URL in config.js
    V.2

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   APP_NAME: "ระบบรายงานความก้าวหน้า KM มหาวิทยาลัยราชภัฏยะลา",
   APP_SHORT_NAME: "KM YRU Progress",
   APP_SUBTITLE: "Knowledge Management Progress Reporting System",
-  GAS_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycby2dWn0_HYGOxbREPp5DOzrovPIoCx5nFSUmh1Z-JZ1o3tKpaz4BavKftCp_7mgx0pc/exec",
+  GAS_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbxilvjjar8fmUIVKc3_hGokjWDJDk7gyH5RjQki648qxbV2_DSsHr2B3vFLhDxiS4e3/exec",
   THEME_COLOR: "#6c3bbd",
   THEME_COLOR_2: "#8f5de8",
   SHOW_TOP_BRAND: true,
