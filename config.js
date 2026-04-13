eabde8c2d7d4cf0ed55b9c67a0097c3f9266bf3a
sofina-dev <81674971+sofina-dev@users.noreply.github.com>
2026-04-13 08:23:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
   APP_NAME: "ระบบรายงานความก้าวหน้า KM มหาวิทยาลัยราชภัฏยะลา",
   APP_SHORT_NAME: "KM YRU Progress",
   APP_SUBTITLE: "Knowledge Management Progress Reporting System",
-  GAS_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbz6cNswXtuIwLtPbmlz3x5S8qW6F9bPzdRSWXJHbE7A3gGgOzojhXgBV6cvFtYks620/exec",
+  GAS_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbwogjoEBPfyszGTYUQKjDzMybXQAhqbNu9UDiRlasrj-K5TQGWTLM-UZOpkssaroXoUDg/exec",
   THEME_COLOR: "#6c3bbd",
   THEME_COLOR_2: "#8f5de8",
   LOADING_TEXT: "กำลังเชื่อมต่อระบบ..."
