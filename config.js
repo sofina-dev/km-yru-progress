83265ae28404fc333913c71213260320aec17b5c
sofina-dev <sofina.dev@gmail.com>
2026-06-22 16:22:59 +0700

    Invalidate stale local Web App URLs

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.PORTAL_CONFIG = {
+  CONFIG_VERSION: "2026-06-22-innovation-2",
   KM_WEB_APP_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbwogjoEBPfyszGTYUQKjDzMybXQAhqbNu9UDiRlasrj-K5TQGWTLM-UZOpkssaroXoUDg/exec",
   INNOVATION_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzWJsRWKmVmuWqxTnhLmOnWPy-zQAHVu_ASEkYvijoFKsdYXgYP8r4nfoXSb86r_XMSTg/exec",
   LOGO_URL: "assets/logo.jpg"
