2b5ef839f646c16b841bfab7297f538762f07fd5
sofina-dev <sofina.dev@gmail.com>
2026-06-28 14:33:50 +0700

    update URL Innovation

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PORTAL_CONFIG = {
   CONFIG_VERSION: "2026-06-28-webapp-28",
   KM_WEB_APP_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbwogjoEBPfyszGTYUQKjDzMybXQAhqbNu9UDiRlasrj-K5TQGWTLM-UZOpkssaroXoUDg/exec",
-  INNOVATION_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw_CCqNSvCdKlQ78bkmIt6UHnnAEJ3QtrhFOzO8462LABLlRjtmXhQBmgsj6zo2ErpsdA/exec",
+  INNOVATION_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxkKoXggRjutGX4nlIuvnEtTRQ-Mr0jsBuxW1KoQ8Ah0PxTQsG9w6t-RR8PzYzXN5xaAA/exec",
   LOGO_URL: "assets/logo.jpg"
 };
