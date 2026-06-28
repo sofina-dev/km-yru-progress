076897a33cf79ff404af318c480d51655e394c31
sofina-dev <sofina.dev@gmail.com>
2026-06-28 12:17:01 +0700

    Bust stale innovation admin config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PORTAL_CONFIG = {
-  CONFIG_VERSION: "2026-06-22-innovation-2",
+  CONFIG_VERSION: "2026-06-28-admin-login-1",
   KM_WEB_APP_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbwogjoEBPfyszGTYUQKjDzMybXQAhqbNu9UDiRlasrj-K5TQGWTLM-UZOpkssaroXoUDg/exec",
   INNOVATION_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzWJsRWKmVmuWqxTnhLmOnWPy-zQAHVu_ASEkYvijoFKsdYXgYP8r4nfoXSb86r_XMSTg/exec",
   LOGO_URL: "assets/logo.jpg"
