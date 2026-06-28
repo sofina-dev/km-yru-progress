7084e1d436e06cee94e08263e4b97d500fa14dda
sofina-dev <sofina.dev@gmail.com>
2026-06-28 13:29:55 +0700

    Point innovation portal to active web app deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PORTAL_CONFIG = {
-  CONFIG_VERSION: "2026-06-28-admin-login-1",
+  CONFIG_VERSION: "2026-06-28-webapp-28",
   KM_WEB_APP_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbwogjoEBPfyszGTYUQKjDzMybXQAhqbNu9UDiRlasrj-K5TQGWTLM-UZOpkssaroXoUDg/exec",
-  INNOVATION_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzWJsRWKmVmuWqxTnhLmOnWPy-zQAHVu_ASEkYvijoFKsdYXgYP8r4nfoXSb86r_XMSTg/exec",
+  INNOVATION_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbw_CCqNSvCdKlQ78bkmIt6UHnnAEJ3QtrhFOzO8462LABLlRjtmXhQBmgsj6zo2ErpsdA/exec",
   LOGO_URL: "assets/logo.jpg"
 };
