241fe662c1ca44f9b3582a4fbe49bd48d96356ac
sofina-dev <sofina.dev@gmail.com>
2026-08-02 09:59:46 +0700

    Bump innovation portal config version

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 if (typeof window !== 'undefined') {
   window.PORTAL_CONFIG = {
-    CONFIG_VERSION: "2026-08-02-webapp-41",
+    CONFIG_VERSION: "2026-08-02-webapp-42",
     KM_WEB_APP_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbwogjoEBPfyszGTYUQKjDzMybXQAhqbNu9UDiRlasrj-K5TQGWTLM-UZOpkssaroXoUDg/exec",
     INNOVATION_WEB_APP_URL: "https://script.google.com/a/macros/yru.ac.th/s/AKfycbxC696kvX5f8_MbEzvtiL135c1Zjhq3FAyDErqCBTd6FgpVD1iOZdcHvn1oazbVR3nWFw/exec",
     LOGO_URL: "assets/logo.jpg"
